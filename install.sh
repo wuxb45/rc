11cc3eccb12575e872a479114f3c074f36d5876e
Wu Xingbo <wuxb45@gmail.com>
2011-12-30 10:41:59 +0800

    add terminator config

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# normal .xx files
 echo "install all config files into your HOME."
 for cf in $(cat config.list);
 do
@@ -6,3 +8,7 @@ do
   install "$cf" "$HOME/.$cf"
 done
 
+# special files
+# terminator
+mkdir -p ~/.config/terminator
+cp terminator-config ~/.config/terminator/config
