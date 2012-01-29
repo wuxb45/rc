330d1fe9bdd0637c7882bad7f1b185f9203a34bb
Wu Xingbo <wuxb45@gmail.com>
2012-01-29 23:53:40 +0800

    add config for adobe reader

--- install.sh
+++ install.sh
@@ -8,8 +8,12 @@ do
   install "$cf" "$HOME/.$cf"
 done
 
-# special files
+### special files
 # terminator
 echo "terminator-config -> ~/.config/terminator/config"
 mkdir -p ~/.config/terminator
 cp terminator-config ~/.config/terminator/config
+
+# Adobe Reader 9.0
+mkdir -p ~/.adobe/Acrobat/9.0/Preferences
+cp reader_prefs ~/.adobe/Acrobat/9.0/Preferences/
