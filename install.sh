aa6beb5ca557268d551710e680be1f33d1c39ff2
Wu Xingbo <wuxb45@gmail.com>
2012-05-22 10:13:29 +0800

    use rsync to update/install files

--- install.sh
+++ install.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
+put ()
+{
+  rsync -u $1 $2
+}
 
 # normal .xx files
 echo "install all config files into your HOME."
 for cf in $(cat config.list);
 do
   echo "$cf -> $HOME/.$cf"
-  cp "$cf" "$HOME/.$cf"
+  put "$cf" "$HOME/.$cf"
 done
 
 ### special files
 # terminator
 echo "terminator-config -> ~/.config/terminator/config"
 mkdir -p ~/.config/terminator
-cp terminator-config ~/.config/terminator/config
+put terminator-config ~/.config/terminator/config
 
 # Adobe Reader 9.0
 echo "reader_prefs -> ~/.adobe/Acrobat/9.0/Preferences/"
 mkdir -p ~/.adobe/Acrobat/9.0/Preferences
-cp reader_prefs ~/.adobe/Acrobat/9.0/Preferences/
+put reader_prefs ~/.adobe/Acrobat/9.0/Preferences/
 
 # Cabal
 echo "cabal-config -> ~/.cabal/config"
 mkdir -p ~/.cabal
-cp cabal-config ~/.cabal/config
+put cabal-config ~/.cabal/config
 
 # Yong input method
 echo "yong.ini -> ~/.yong/yong.ini"
 mkdir -p ~/.yong
-cp yong.ini ~/.yong/yong.ini
+put yong.ini ~/.yong/yong.ini
 
 # vim filetype.vim
 echo "filetype.vim -> ~/.vim/filetype.vim"
 mkdir -p ~/.vim
-cp filetype.vim ~/.vim/filetype.vim
+put filetype.vim ~/.vim/filetype.vim
