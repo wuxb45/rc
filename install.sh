c31073030f2557193c120c9bad7137633d3f6a61
Wu Xingbo <wuxb45@gmail.com>
2013-02-05 19:39:56 +0800

    add systemtap vim files; cleanup install script

--- install.sh
+++ install.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 put ()
 {
+  echo "${1} -> ${2}"
   rsync -u $1 $2
 }
 
 # normal .xx files
-echo "install all config files into your HOME."
+#echo "install all config files into your HOME."
+
 for cf in $(cat config.list);
 do
-  echo "$cf -> $HOME/.$cf"
   put "$cf" "$HOME/.$cf"
 done
 
 ### special files
 # terminator
-echo "terminator-config -> ~/.config/terminator/config"
 mkdir -p ~/.config/terminator
 put terminator-config ~/.config/terminator/config
 
 # Adobe Reader 9.0
-echo "reader_prefs -> ~/.adobe/Acrobat/9.0/Preferences/"
 mkdir -p ~/.adobe/Acrobat/9.0/Preferences
 put reader_prefs ~/.adobe/Acrobat/9.0/Preferences/
 
 # Cabal
-echo "cabal-config -> ~/.cabal/config"
 mkdir -p ~/.cabal
 put cabal-config ~/.cabal/config
 
 # Yong input method
-echo "yong.ini -> ~/.yong/yong.ini"
 mkdir -p ~/.yong
 put yong.ini ~/.yong/yong.ini
 
 # vim
 mkdir -p ~/.vim/undodir
 mkdir -p ~/.vim/ftdetect
-put vim/ftdetect/hsc.vim ~/.vim/ftdetect/hsc.vim
+put vim/ftdetect/hsc.vim   ~/.vim/ftdetect/hsc.vim
 put vim/ftdetect/scala.vim ~/.vim/ftdetect/scala.vim
+put vim/ftdetect/stap.vim  ~/.vim/ftdetect/stap.vim
 mkdir -p ~/.vim/syntax
-put vim/syntax/scala.vim ~/.vim/syntax/scala.vim
+put vim/syntax/scala.vim   ~/.vim/syntax/scala.vim
+put vim/syntax/stap.vim    ~/.vim/syntax/stap.vim
 mkdir -p ~/.vim/indent
-put vim/indent/scala.vim ~/.vim/indent/scala.vim
-
+put vim/indent/scala.vim   ~/.vim/indent/scala.vim
+put vim/indent/stap.vim    ~/.vim/indent/stap.vim
+mkdir -p ~/.vim/ftplugin
+put vim/ftplugin/stap.vim  ~/.vim/ftplugin/stap.vim
