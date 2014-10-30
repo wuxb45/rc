12ddd9bc4b951dab191198f179ee2578f65c9aa2
Wu Xingbo <wuxb45@gmail.com>
2014-10-29 22:01:49 -0400

    install to DEST

--- install.sh
+++ install.sh
@@ -12,44 +12,46 @@ put ()
 
 # normal .xx files
 #echo "install all config files into your HOME."
+DEST=${DEST:-${HOME}}
+echo "[DEST=${DEST}]"
 
 for cf in $(cat config.list);
 do
-  put "$cf" "$HOME/.$cf"
+  put "$cf" "$DEST/.$cf"
 done
 
 ### special files
 # terminator
-mkdir -p ~/.config/terminator
-put terminator-config ~/.config/terminator/config
+mkdir -p ${DEST}/.config/terminator
+put terminator-config ${DEST}/.config/terminator/config
 
 # Adobe Reader 9.0
-#mkdir -p ~/.adobe/Acrobat/9.0/Preferences
-#put reader_prefs ~/.adobe/Acrobat/9.0/Preferences/
+#mkdir -p ${DEST}/.adobe/Acrobat/9.0/Preferences
+#put reader_prefs ${DEST}/.adobe/Acrobat/9.0/Preferences/
 
 # Cabal
-#mkdir -p ~/.cabal
-#put cabal-config ~/.cabal/config
+#mkdir -p ${DEST}/.cabal
+#put cabal-config ${DEST}/.cabal/config
 
 # Yong input method
-#mkdir -p ~/.yong
-#put yong.ini ~/.yong/yong.ini
+#mkdir -p ${DEST}/.yong
+#put yong.ini ${DEST}/.yong/yong.ini
 
 # matplotlib set default backend to svg
-mkdir -p ~/.config/matplotlib
-put matplotlibrc ~/.config/matplotlib/matplotlibrc
+mkdir -p ${DEST}/.config/matplotlib
+put matplotlibrc ${DEST}/.config/matplotlib/matplotlibrc
 
 # vim
-mkdir -p ~/.vim/undodir
-mkdir -p ~/.vim/ftdetect
-put vim/ftdetect/hsc.vim   ~/.vim/ftdetect/hsc.vim
-put vim/ftdetect/scala.vim ~/.vim/ftdetect/scala.vim
-put vim/ftdetect/stap.vim  ~/.vim/ftdetect/stap.vim
-mkdir -p ~/.vim/syntax
-put vim/syntax/scala.vim   ~/.vim/syntax/scala.vim
-put vim/syntax/stap.vim    ~/.vim/syntax/stap.vim
-mkdir -p ~/.vim/indent
-put vim/indent/scala.vim   ~/.vim/indent/scala.vim
-put vim/indent/stap.vim    ~/.vim/indent/stap.vim
-mkdir -p ~/.vim/ftplugin
-put vim/ftplugin/stap.vim  ~/.vim/ftplugin/stap.vim
+mkdir -p ${DEST}/.vim/undodir
+mkdir -p ${DEST}/.vim/ftdetect
+put vim/ftdetect/hsc.vim   ${DEST}/.vim/ftdetect/hsc.vim
+put vim/ftdetect/scala.vim ${DEST}/.vim/ftdetect/scala.vim
+put vim/ftdetect/stap.vim  ${DEST}/.vim/ftdetect/stap.vim
+mkdir -p ${DEST}/.vim/syntax
+put vim/syntax/scala.vim   ${DEST}/.vim/syntax/scala.vim
+put vim/syntax/stap.vim    ${DEST}/.vim/syntax/stap.vim
+mkdir -p ${DEST}/.vim/indent
+put vim/indent/scala.vim   ${DEST}/.vim/indent/scala.vim
+put vim/indent/stap.vim    ${DEST}/.vim/indent/stap.vim
+mkdir -p ${DEST}/.vim/ftplugin
+put vim/ftplugin/stap.vim  ${DEST}/.vim/ftplugin/stap.vim
